88591ffd1d834f0b6d5abc3dd6a2b67f26cb60f1
codevs <contact.codevs@gmail.com>
2024-02-03 01:14:13 +0200

    change loader

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,7 +1,11 @@
+import Image from "next/image";
 function Loading() {
   return (
     <div className="py-24 px-8 md:px-24 lg:px-36 xl:px-48 2xl:px-72 bg-neutral-950 text-white p-4 min-h-screen flex flex-col  justify-center items-center">
-      <img
+      <Image
+        priority
+        width={500}
+        height={500}
         src="/logo.svg"
         className=" bg-gray-500/10 rounded-md p-4  md:w-1/12 w-5/12 mx-auto animate-[ping_3s_ease-in-out_infinite] transition-all "
       />
