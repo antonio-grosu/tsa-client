b0041731c69f92f2734a8c1bd25c00ab9a81410b
tunder007 <94933261+tunder007@users.noreply.github.com>
2024-02-10 13:15:37 +0200

    Create test.js

---
+++ app/test.js
@@ -0,0 +1,94 @@
+import Image from "next/image"
+const Test = (props) => (
+    <div className="h-screen bg-neutral-950 p-1 flex items-center justify-center poppins">
+        <div className="bg-neutral-500/20 md:w-1/2 flex flex-col items-center justify-between rounded-md gap-24 md:gap-12 p-8 z-50">
+            <div className="flex flex-col justify-start  w-full gap-4">
+                <div className="flex items-center justify-start gap-6 text-white">
+                    <div className="md:w-24 md:h-24 w-1/3">
+                        <Image src="/bac.svg" alt="BAC" width={32} height={32} className="w-full border-2" />
+                    </div>
+                    <div>
+                        <h3 className="text-2xl text-lime-300 font-bold">Aritmetică și Operatori Liniari</h3>
+                        <p className="text-sm underline underline-offset-2 mt-2">N-am chef de BAC, ce fac?</p>
+                    </div>
+                </div>
+                <div className="grid  grid-cols-12 w-full bg-lime-900 p-2 rounded-full">
+                    <div className="w-full bg-lime-300 rounded-l-full relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2"><svg className="w-8 h-8 z-10 text-sky-600 absolute -right-2 -bottom-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z" clip-rule="evenodd" />
+                    </svg>
+
+                    </div>
+                    <div className="w-full bg-lime-300 relative p-2">
+                        <div className="w-8 h-8 z-10 bg-green-900 rounded-full flex items-center justify-center text-sky-600 absolute -right-2 -bottom-2">
+                            <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                <path fill-rule="evenodd" d="M20.3 3.7c.2.2.4.4.4.7.3 1.8.7 5.2-.9 6.8A75.2 75.2 0 0 1 8.6 18a1 1 0 0 1-.6-.3l-.8-.9-1-.8a1 1 0 0 1 0-1.2c1-2.2 4.8-8.9 6.6-10.6 1.6-1.6 5-1.2 6.8-1l.7.5ZM5.4 7.6l4-.4-2.7 4.5-2.8-.3a1 1 0 0 1-.6-1.7l2.1-2.1Zm11.4 7-.4 4-2 2.1a1 1 0 0 1-1.8-.6l-.4-2.8 4.6-2.7Zm.8-6.2a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" clip-rule="evenodd" />
+                            </svg>
+
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            </div>
+            <div className="flex flex-col gap-4 text-white md:w-10/12 md:px-12">
+                <p className="bg-white/20 p-4 text-sm rounded-md ">Termeni grei, știu, dar cu o explicație mult mai simplă. Ei de fapt reprezintă operații simple de matematică, cum ar fi adunarea, scăderea, înmulțirea și împărțirea. În cazul operatorilor liniari, se adaugă și împărțirea cu rest.s</p>
+                <p className="bg-white/20 p-4 text-sm rounded-md ">Cum era aia... "Nu știu ce e aia împărțire cu rest, dar știu că era simplă"? Ei bine, nu e chiar așa de simplă, dar nici foarte grea.</p>
+
+            </div>
+            <button className="w-full md:w-1/2 bg-gradient-to-r from-sky-600 to-lime-300 font-bold text-lg py-2 rounded-full border-2 border-white text-white ">Next + 20 XP</button>
+        </div>
+    </div>
+)
+
+export default Test
